5a9aa3dc4aa97eb15f2c7416a9494bfee90d3d0b
classyPimp <mussa88@mail.ru>
2017-12-22 11:32:53 +0600

    before circ dep bug

--- assets/javascript/index.tsx
+++ assets/javascript/index.tsx
@@ -1,17 +1,5 @@
+import { ModelRegistrySingleton } from './modelLayer/ModelRegistry'
 import { initApplication } from './application/initApplication';
-import { Route, RequestOptions } from './modelLayer/annotations/ModelRoute';
-import { ModelCollection } from './modelLayer/ModelCollection';
-import { HasOne } from './modelLayer/annotations/HasOne';
-import { Property } from './modelLayer/annotations/Property';
-import { BaseModel } from './modelLayer/BaseModel';
-import { HasMany } from './modelLayer/annotations/HasMany';
-import { ModelSerializer } from './modelLayer/ModelSerializer';
-import { Promise } from 'es6-promise';
-import { IModelProperties } from './modelLayer/interfaces/IModelProperties'
-import { DefferedPromise } from './modelLayer/utils/DeferredPromise'
-import { User } from './application/models/User';
- 
-
 // let collection = ModelSerializer.parseCollection<User>(
 //     User, 
 //     [
